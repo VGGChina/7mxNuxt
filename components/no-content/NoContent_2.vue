898ada517948e8ec68732a1bf330f2cbce676ca4
Ricardotang <1604637185@qq.com>
2019-11-25 17:58:43 +0800

    1

--- components/no-content/NoContent_2.vue
+++ components/no-content/NoContent_2.vue
@@ -1,7 +1,10 @@
 <template>
   <transition name="no-content-fade">
     <div v-if="isNoContentShow" class="no-content">
-      <div :style="{ 'height': `${textHeight * 40}px` }" class="content">{{ content || 'sorry，暂无内容，您可以尝试刷新页面' }}</div>
+      <div
+        :style="{ 'height': `${textHeight * 40}px` }"
+        class="content"
+      >{{ content || 'sorry，暂无内容，您可以尝试刷新页面' }}</div>
       <img src="./img/paixin_cute_0.png" width="50" alt="">
       <img src="./img/paixin_cute_1.png" width="80" alt="">
     </div>
@@ -10,10 +13,7 @@
 
 <script>
 export default {
-  props: [
-    'isNoContentShow',
-    'content'
-  ],
+  props: ['isNoContentShow', 'content'],
   computed: {
     textHeight() {
       if (this.content) {
@@ -29,7 +29,7 @@ export default {
 <style lang='scss' scoped>
 .no-content-fade-enter-active,
 .no-content-fade-leave-active {
-  transition: all .3s;
+  transition: all 0.3s;
 }
 
 .no-content-fade-enter,
@@ -43,7 +43,7 @@ export default {
     margin: 0 auto;
   }
   .content {
-    color: rgba(0,0,0,0.5);
+    color: rgba(0, 0, 0, 0.5);
     margin: 0 auto;
     height: 40px;
     width: 200px;
@@ -51,9 +51,8 @@ export default {
     font-size: 1rem;
     text-align: center;
     border-radius: 10px;
-    padding: 10px 12px;
-    background: rgba(0,0,0,0.06);
+    // padding: 10px 12px;
+    background: rgba(0, 0, 0, 0.06);
   }
 }
-
 </style>
